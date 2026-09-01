2ed87c3fcc33f984267cba8d404b36eca2ac5ebf
Owen6936TH <74977423+Owen6936TH@users.noreply.github.com>
2026-09-01 17:10:56 +0700

    Complete Assignment

--- Assets/Scripts/Workspace/Assignment/StudentSolution/Homework/Player.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution/Homework/Player.cs
@@ -2,8 +2,21 @@ using UnityEngine;
 
 namespace Assignment.StudentSolution
 {
-    public class Player
+    public class Player : Entity
     {
+        public int score;
+        private Item[] items = new Item[0];
 
+        public void CollectItem(Item item)
+        {
+            var collectedItems = new Item[items.Length + 1];
+            items.CopyTo(collectedItems, 0);
+            collectedItems[items.Length] = item;
+            items = collectedItems;
+        }
+
+        protected void LevelUp()
+        {
+        }
     }
 }
